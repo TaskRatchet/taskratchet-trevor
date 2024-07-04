a4baa3aed2fa62174c6d4d7049d4fe460ba75872
Nathan Arthur <nathan@nathanarthur.com>
2024-07-04 11:49:27 -0400

    add beeminder blog to vector store

--- scripts/update-knowledge.ts
+++ scripts/update-knowledge.ts
@@ -1,7 +1,9 @@
 import fs from "fs";
 import path from "path";
-import updateVectorStore from "../src/services/openai/updateVectorStore.js";
+import replaceVectoryStore from "../src/services/openai/replaceVectorStore.js";
 import "dotenv/config";
+import { Uploadable, toFile } from "openai/uploads.mjs";
+import pLimit from "p-limit";
 
 function getAllMdFiles(dirPath: string, arrayOfFiles: string[] = []) {
   const files = fs.readdirSync(dirPath);
@@ -20,8 +22,37 @@ function getAllMdFiles(dirPath: string, arrayOfFiles: string[] = []) {
 
 const mdFiles = getAllMdFiles("./submodules/taskratchet.com");
 
-console.log(mdFiles);
-
 const streams = mdFiles.map((f) => fs.createReadStream(f));
 
-await updateVectorStore("taskratchet.com", streams);
+const blogConfig = await fetch(
+  "https://raw.githubusercontent.com/beeminder/blog/master/posts.json",
+).then((r) => r.json());
+
+const sources: string[] = blogConfig.map((b: Record<string, unknown>) => {
+  const url = new URL(`https://${b.source}`);
+  url.hostname = process.env.ETHERPAD_DOMAIN || "";
+  url.pathname = url.pathname + "/export/txt";
+  return url.toString();
+});
+
+const limit = pLimit(1);
+
+const uploadables: Uploadable[] = await Promise.all(
+  sources.map((s, i) => {
+    const pathname = new URL(s).pathname;
+    const segments = pathname.split("/").filter(Boolean);
+    const slug = segments[0];
+    return toFile(
+      limit(async () => {
+        const k = `${i + 1}/${sources.length}: ${slug}`;
+        console.time(k);
+        const r = await fetch(s);
+        console.timeEnd(k);
+        return r;
+      }),
+      `${slug}.md`,
+    );
+  }),
+);
+
+await replaceVectoryStore("knowledge", [...streams, ...uploadables]);
---
+++ src/services/openai/replaceVectorStore.ts
@@ -0,0 +1,28 @@
+import getClient from "./getClient.js";
+import deleteVectorStore from "./deleteVectorStore.js";
+import { Uploadable } from "openai/uploads.mjs";
+
+export default async function replaceVectoryStore(
+  storeName: string,
+  files: Uploadable[],
+) {
+  const c = getClient();
+
+  await deleteVectorStore(storeName);
+
+  const store = await c.beta.vectorStores.create({
+    name: storeName,
+  });
+
+  await c.beta.vectorStores.fileBatches.uploadAndPoll(store.id, {
+    files,
+  });
+
+  await c.beta.assistants.update(process.env.OPENAI_ASSISTANT_ID || "", {
+    tool_resources: {
+      file_search: {
+        vector_store_ids: [store.id],
+      },
+    },
+  });
+}
